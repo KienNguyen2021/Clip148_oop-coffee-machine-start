dc0ffa7d97c3edcd4ef1a118a80778ad53122927
nguyen8k <56096127+nguyen8k@users.noreply.github.com>
2021-05-11 22:54:11 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+# create an object       class moneyMachine:
+money_machine = MoneyMachine ()
+
+coffee_maker = CoffeeMaker()
+
+coffee_maker.report()
+# an object  call a method :
+money_machine.report()
